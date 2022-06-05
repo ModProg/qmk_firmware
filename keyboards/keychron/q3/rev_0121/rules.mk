180132fb70f67ac1d055d8ecfaf25728da0dee09
Floris Westerman <me@floriswesterman.nl>
2022-06-05 16:29:36 +0200

    Attempt at incorporating EEPROM and rotary encoder fixes

--- keyboards/keychron/q3/rev_0121/rules.mk
+++ keyboards/keychron/q3/rev_0121/rules.mk
@@ -20,7 +20,6 @@ ENCODER_ENABLE = yes        # Enable Encoder
 DIP_SWITCH_ENABLE = yes
 RGB_MATRIX_ENABLE = yes
 RGB_MATRIX_DRIVER = CKLED2001
-EEPROM_DRIVER = transient
 
 # Enter lower-power sleep mode when on the ChibiOS idle thread
 OPT_DEFS += -DCORTEX_ENABLE_WFI_IDLE=TRUE
