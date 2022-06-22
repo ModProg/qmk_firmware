d9f883130fb89ce34bbd965a5048623de38ee031
Floris Westerman <me@floriswesterman.nl>
2022-06-23 01:02:34 +0200

    Incorporate change from Keychron upstream

--- keyboards/keychron/q3/rev_0121/rules.mk
+++ keyboards/keychron/q3/rev_0121/rules.mk
@@ -21,6 +21,7 @@ ENCODER_MAP_ENABLE = no		# Disable Encoder map
 DIP_SWITCH_ENABLE = yes
 RGB_MATRIX_ENABLE = yes
 RGB_MATRIX_DRIVER = CKLED2001
+RAW_ENABLE = yes
 
 # Enter lower-power sleep mode when on the ChibiOS idle thread
 OPT_DEFS += -DCORTEX_ENABLE_WFI_IDLE=TRUE
