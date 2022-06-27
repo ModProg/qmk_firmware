a49737e27128f21cbc99352958a213fbe5edd779
Roland Fredenhagen <dev@modprog.de>
2022-06-27 10:50:25 +0200

    keymap 'done'

--- keyboards/planck/keymaps/modprog/keymap.c
+++ keyboards/planck/keymaps/modprog/keymap.c
@@ -26,6 +26,8 @@
 enum planck_layers {
   _QWERTZ_BASE,
   _QWERTZ_M1,
+  _QWERTZ_M2,
+  _QWERTZ_DEU
 };
 
 enum planck_keycodes {
@@ -42,6 +44,7 @@ const key_override_t **key_overrides = (const key_override_t *[]){
 	&ko_make_basic(MOD_MASK_ALT  , DE_E   , DE_EURO),
     // Layer -1
 	&ko_make_basic(MOD_MASK_SHIFT, DE_CIRC, DE_DEG),
+	&ko_make_basic(MOD_MASK_SHIFT, DE_DQUO, DE_QUOT),
 	// &ko_make_basic(MOD_MASK_SHIFT, DE_COMM, DE_LABK),
 	// &ko_make_basic(MOD_MASK_SHIFT, DE_DOT , DE_RABK),
 	// &ko_make_basic(MOD_MASK_SHIFT, DE_SCLN, DE_COLN),
@@ -53,7 +56,9 @@ const key_override_t **key_overrides = (const key_override_t *[]){
 };
 
 #define Z_M1 MO(_QWERTZ_M1)
+#define Z_M2 MO(_QWERTZ_M2)
 #define Z_M1_OS LM(_QWERTZ_M1, MOD_LGUI)
+#define Z_DEU OSL(_QWERTZ_DEU)
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Qwertz Base
@@ -69,18 +74,36 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_QWERTZ_BASE] = LAYOUT_planck_grid(
     KC_TAB,  DE_Q,    DE_W,    DE_E,    DE_R,    DE_T,    DE_Z,    DE_U,    DE_I,    DE_O,    DE_P,    KC_BSPC,
-    KC_ESC,  DE_A,    DE_S,    DE_D,    DE_F,    DE_G,    DE_H,    DE_J,    DE_K,    DE_L,    DE_QUES, OSL(_QWERTZ_M1),
+    KC_ESC,  DE_A,    DE_S,    DE_D,    DE_F,    DE_G,    DE_H,    DE_J,    DE_K,    DE_L,    DE_QUES, Z_DEU,
     KC_LSFT, DE_Y,    DE_X,    DE_C,    DE_V,    DE_B,    DE_N,    DE_M,    DE_COMM, DE_DOT,  DE_MINS, KC_ENT ,
-    KC_LCTL, KC_LCTL, KC_LGUI, KC_LALT, Z_M1   , KC_SPC,  KC_SPC,  Z_M1_OS, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+    KC_LCTL, Z_M2   , KC_LGUI, KC_LALT, Z_M1   , KC_SPC,  KC_SPC,  Z_M1_OS, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
 ),
 
 /* Qwertz -1
  */
 [_QWERTZ_M1] = LAYOUT_planck_grid(
     DE_CIRC, DE_1   , DE_2   , DE_3   , DE_4   , DE_5   , DE_6   , DE_7   , DE_8   , DE_9   , DE_0   , KC_DEL, 
-    DE_LPRN, DE_RPRN, DE_LBRC, DE_RBRC, DE_QUOT, DE_DQUO, KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, DE_ASTR, DE_TILD, 
+    KC_LGUI, DE_LPRN, DE_RPRN, DE_LBRC, DE_RBRC, DE_DQUO, KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, DE_ASTR, DE_TILD, 
     _______, DE_DLR,  DE_PERC, DE_AMPR, DE_SLSH, DE_PIPE, DE_LCBR, DE_RCBR, DE_LABK, DE_RABK, DE_PLUS, DE_HASH, 
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, DE_EQL , _______ 
+),
+
+/* Qwertz -2
+ */
+[_QWERTZ_M2] = LAYOUT_planck_grid(
+    KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 , KC_F12 ,             
+    KC_LGUI, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, DE_BSLS, _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ 
+),
+
+/* Qwertz DEU
+ */
+[_QWERTZ_DEU] = LAYOUT_planck_grid(
+    _______, _______, _______, _______, _______, _______, _______, DE_UDIA, _______, DE_ODIA, _______, _______,
+    _______, DE_ADIA, DE_SS  , _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ 
 ),
 
 };
